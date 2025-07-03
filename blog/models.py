5a01848e243798f9a044e9eed58ebf514217c103
Yoon- Hajeong <pionksribboon@gmail.com>
2025-07-03 11:40:52 +0900

    카테고리 설정하기

--- blog/models.py
+++ blog/models.py
@@ -2,9 +2,22 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    CATEGORY_CHOICES = [
+        ('diary', '영어일기'),
+        ('speaking', '영어회화'),
+        ('personal_feedback', '학습점검'),
+    ]
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
     content = models.TextField()
+    category = models.CharField(max_length=20, choices=CATEGORY_CHOICES)
 
+    category = models.CharField(
+        max_length=20,
+        choices=CATEGORY_CHOICES,
+        default='diary'
+    )
+    
     def __str__(self):
         return self.title
