886b619f0d1e436f98ab5699cb7e63caacb28c5f
Yoon- Hajeong <pionksribboon@gmail.com>
2025-07-03 12:26:04 +0900

    글작성 초안

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('blog', views.blog_list, name='blog_list'),
     path('blog/<int:id>', views.blog_detail, name='blog_detail'),
+    path('write/', views.post_write, name='post_write'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,19 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Post
+from .forms import PostForm
 
 def home(request):
     return render(request, 'blog/home.html')
+
+def post_write(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('/blog/')
+    else:
+        form = PostForm()
+    return render(request, 'blog/post_write.html', {'form': form})
 
 def blog_list(request):
     posts = Post.objects.all()
